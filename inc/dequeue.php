72b754c0e5daebddae0ec7bf4a5ebebb26195934
Chris Schwartze <ch.schwartze@gmail.com>
2018-04-30 14:18:00 +0200

    Clean up

--- inc/dequeue.php
+++ inc/dequeue.php
@@ -6,19 +6,6 @@
 function dequeue_unnecessary_scripts()
 {
 
-    if ( is_checkout() ) {
-        wp_dequeue_script('x-woocommerce');
-        wp_deregister_script('x-woocommerce');
-    }
-    if ( !is_checkout() ) {
-        wp_dequeue_script('tco_woo_scripts');
-        wp_deregister_script('tco_woo_scripts');
-    }
-    if ( !is_single() ) {
-        wp_dequeue_script('prismatic-prism');
-        wp_deregister_script('prismatic-prism');
-    }
-
 }
 add_action('wp_print_scripts', 'dequeue_unnecessary_scripts');
 
@@ -29,18 +16,6 @@ add_action('wp_print_scripts', 'dequeue_unnecessary_scripts');
 function dequeue_unnecessary_styles()
 {
 
-    if ( !is_admin() ) {
-        wp_deregister_style('duplicate-post');
-    }
-    if ( !is_checkout() ) {
-        wp_deregister_style('tco_woo_front_css');
-    }
-    if ( !is_single() ) {
-        wp_deregister_style('prismatic-prism');
-    }
-    if ( !is_checkout() || !is_cart() || !is_product() || !is_account_page() || !is_page('cart') ) {
-        //wp_deregister_style('x-woocommerce');
-    }
 
 }
 add_action('wp_print_styles', 'dequeue_unnecessary_styles', 100);
